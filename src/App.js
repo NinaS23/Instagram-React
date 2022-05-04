ec7548a1a1e2640da419af375892898a35662583
MariaDuda <mariabevenuto062@gmail.com>
2022-05-04 13:21:26 -0300

    stories done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "../public/css/reset.css";
+import "../public/css/style.css";
+import TopoDoSite from "./componentes/Topo";
+
+export default function App(){
+    return (
+        <>
+        <TopoDoSite />
+        
+        </>
+    )
+}
